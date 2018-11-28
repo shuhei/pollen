dd2c7b71e581b8d62f1c932de60fd9b67a697522
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2018-11-28 01:08:51 +0100

    Test DnsPolling class

--- __tests__/dns-polling.js
+++ __tests__/dns-polling.js
@@ -1,10 +1,46 @@
-const { DnsPolling } = require('..');
+jest.mock('../poller', () => {
+  const EventEmitter = require('events');
+  class MockPoller extends EventEmitter {
+  }
+  MockPoller.prototype.start = jest.fn().mockReturnThis();
+  MockPoller.prototype.stop = jest.fn();
+  MockPoller.prototype.getLookup = jest.fn();
+  return MockPoller;
+});
+
+const { DnsPolling, Poller } = require('..');
 
 describe('DnsPolling', () => {
-  it('instantiates', () => {
+  it('calls start, getLookup and stop of poller', () => {
     const polling = new DnsPolling();
+
     const lookupFoo = polling.getLookup('foo.com');
     const lookupBar = polling.getLookup('bar.com');
+    expect(Poller.prototype.start).toHaveBeenCalledTimes(2);
+    expect(Poller.prototype.getLookup).toHaveBeenCalledTimes(2);
+
     polling.stop();
+    expect(Poller.prototype.stop).toHaveBeenCalledTimes(2);
+  });
+
+  it('forwards events from pollers', (done) => {
+    const polling = new DnsPolling();
+    const successPayload = { dummy: 'success' };
+    const errorPayload = { dummy: 'error' };
+    let successCalled = false;
+    polling.once('resolve:success', (payload) => {
+      expect(payload).toBe(successPayload);
+      successCalled = true;
+    });
+    polling.once('resolve:error', (payload) => {
+      expect(payload).toBe(errorPayload);
+      expect(successCalled).toBe(true);
+      done();
+    });
+    const lookupFoo = polling.getLookup('foo.com');
+    const poller = polling.pollers.get('foo.com');
+
+    poller.emit('resolve:success', successPayload);
+    poller.emit('resolve:error', errorPayload);
   });
 });
